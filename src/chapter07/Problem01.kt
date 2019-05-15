39eae5afce39e37fa39c920d39782f7e8659a125
iwedding <dlwls5201@naver.com>
2019-05-15 12:20:40 +0900

    add chapter07

---
+++ src/chapter07/Problem01.kt
@@ -0,0 +1,39 @@
+package chapter07
+
+/**
+ * 오리처럼 걷고 꽥꽥어린다면 반드시 오리라고 할 수 없습니다.
+ * 오리 어댑터로 감싼 칠면조일 수도 있습니다...
+ */
+class Problem01 {
+
+    interface Duck {
+        fun quack()
+        fun fly()
+    }
+
+    class MallardDuck: Duck {
+        override fun quack() {
+            println("Quack")
+        }
+
+        override fun fly() {
+            println("I'm flying")
+        }
+    }
+
+    //칠면조를 만나 볼까요?
+    interface Turkey {
+        fun gobble() //칠면조는 골골거리는 소리를 냅니다.
+        fun fly() //칠면도는 날 수 있긴 합니다.
+    }
+
+    class WildTurky: Turkey {
+        override fun gobble() {
+            println("Gobble gobble")
+        }
+
+        override fun fly() {
+            println("I'm flying a short distance")
+        }
+    }
+}
---
+++ src/chapter07/Problem02.kt
@@ -0,0 +1,72 @@
+package chapter07
+
+import chapter07.Problem02.*
+
+/**
+ * 오리가 부족하여 칠면조를 대신 사용해야 되는 상홥입니다. 물론 인터페이스가 다르기 때문에 칠면조 객체를 바로 사용할 수는 없습니다.
+ */
+class Problem02 {
+
+    interface Duck {
+        fun quack()
+        fun fly()
+    }
+
+    class MallardDuck: Duck {
+        override fun quack() {
+            println("Quack")
+        }
+
+        override fun fly() {
+            println("I'm flying")
+        }
+    }
+
+    //칠면조를 만나 볼까요?
+    interface Turkey {
+        fun gobble() //칠면조는 골골거리는 소리를 냅니다.
+        fun fly() //칠면도는 날 수 있긴 합니다.
+    }
+
+    class WildTurkey: Turkey {
+        override fun gobble() {
+            println("Gobble gobble")
+        }
+
+        override fun fly() {
+            println("I'm flying a short distance")
+        }
+    }
+
+    //오리를 칠면조로 만들어 주는 어댑터
+    class TurkeyAdapter(val turkey: Turkey): Duck {
+        override fun quack() {
+            turkey.gobble()
+        }
+
+        override fun fly() {
+            for(i in 0..5) {
+                turkey.fly()
+            }
+        }
+    }
+}
+
+fun main() {
+    
+    val duck = MallardDuck()
+    
+    val turkey = WildTurkey()
+    val turkeyAdapter = TurkeyAdapter(turkey)
+    
+    println("--- turkey says")
+    turkey.gobble()
+    turkey.fly()
+
+    println("--- duck says")
+    duck.quack()
+
+    println("--- turkeyAdapter says")
+    turkeyAdapter.quack()
+    turkeyAdapter.fly()
+}
